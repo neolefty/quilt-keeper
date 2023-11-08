3b1e2f937880ddf97825f093759ae91062c3b1b8
Bill Baker <bill@beechersoftware.com>
2023-11-08 11:10:58 -0500

    add pin & trash buttons

---
+++ app/_lib/components/PodgeControls.tsx
@@ -0,0 +1,33 @@
+import { useColorPodge } from "../state/WithColorPodge"
+
+export const PodgeControls = () => {
+    const { setPodge, podge, sortPodge } = useColorPodge()
+    return (
+        <div className="grid grid-cols-4 gap-3">
+            <button
+                className="btn btn-primary"
+                onClick={() => sortPodge(podge.disperse(2))}
+            >
+                Step 2
+            </button>
+            <button
+                className="btn btn-primary"
+                onClick={() => sortPodge(podge.disperse(5))}
+            >
+                Step 5
+            </button>
+            <button
+                className="btn btn-primary"
+                onClick={() => sortPodge(podge.disperse(12))}
+            >
+                Step 12
+            </button>
+            <button
+                className="btn btn-secondary"
+                onClick={() => setPodge(podge.addRandomColor())}
+            >
+                Add
+            </button>
+        </div>
+    )
+}
--- app/_lib/components/ShowPodge.tsx
+++ app/_lib/components/ShowPodge.tsx
@@ -1,49 +1,69 @@
+import Image from "next/image"
 import { useColorPodge } from "../state/WithColorPodge"
+import { PodgeControls } from "./PodgeControls"
+import { DriftColor } from "../color/DriftColor"
+import trash from "../../../public/trash.svg"
+import pinCircle from "../../../public/pin-circle.svg"
+import pinnedCircle from "../../../public/pinned-circle.svg"
+import { useState } from "react"
 
 export const ShowPodge = () => {
     const { podge, setPodge, sortPodge } = useColorPodge()
     return (
         <div className="grid gap-3">
-            <div className="grid grid-cols-4 gap-3">
-                <button
-                    className="btn btn-primary"
-                    onClick={() => sortPodge(podge.disperse(2))}
-                >
-                    Step 2
-                </button>
-                <button
-                    className="btn btn-primary"
-                    onClick={() => sortPodge(podge.disperse(5))}
-                >
-                    Step 5
-                </button>
-                <button
-                    className="btn btn-primary"
-                    onClick={() => sortPodge(podge.disperse(12))}
-                >
-                    Step 12
-                </button>
-                <button
-                    className="btn btn-secondary"
-                    onClick={() => setPodge(podge.addRandomColor())}
-                >
-                    Add
-                </button>
-            </div>
+            <PodgeControls />
             <hr />
-            {podge.driftColors.map((color, i) => (
-                <div
-                    className="card shadow-xl p-3 text-center"
-                    key={i}
-                    style={{
-                        backgroundColor: color.hexString,
-                        color: color.contrast().hexString,
-                    }}
-                    onClick={() => setPodge(podge.removeColor(i))}
-                >
-                    {color.hexString}
-                </div>
+            {podge.driftColors.map((color, idx) => (
+                <PodgeColorCard color={color} idx={idx} key={idx} />
             ))}
+        </div>
+    )
+}
+
+const PodgeColorCard = ({ color, idx }: { color: DriftColor; idx: number }) => {
+    const { podge, setPodge } = useColorPodge()
+    const [pinned, setPinned] = useState(false)
+    return (
+        <div
+            className="card shadow-xl text-center flex flex-row items-center justify-center px-1"
+            key={idx}
+            style={{
+                backgroundColor: color.hexString,
+                color: color.contrast().hexString,
+            }}
+        >
+            <span className="flex-1 pr-3">{color.hexString}</span>
+            <div className="btn btn-ghost px-1">
+                <label className="swap">
+                    <input
+                        type="checkbox"
+                        checked={pinned}
+                        onChange={() => setPinned(!pinned)}
+                    />
+                    <div className="swap-off">
+                        <Image
+                            src={pinCircle}
+                            alt="pin"
+                            width={28}
+                            height={28}
+                        />
+                    </div>
+                    <div className="swap-on">
+                        <Image
+                            src={pinnedCircle}
+                            alt="pin"
+                            width={28}
+                            height={28}
+                        />
+                    </div>
+                </label>
+            </div>
+            <button
+                className="btn btn-ghost px-1"
+                onClick={() => setPodge(podge.removeColor(idx))}
+            >
+                <Image src={trash} alt="delete" width={28} height={28} />
+            </button>
         </div>
     )
 }
