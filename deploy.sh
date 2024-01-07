7ab52160f8fc268e1d0bb1f3080b7d05302ac1c8
Beecher Baker <bbbaker@alum.mit.edu>
2024-01-07 22:49:02 +0000

    install as part of deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 git pull
+pnpm install
 time pnpm build
 sudo cp -r dist/* /var/www/html/neolefty.org/patch-perfect
 sudo chown -R www-data:www-data /var/www/html/neolefty.org/patch-perfect
