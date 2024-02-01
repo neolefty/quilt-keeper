23c25691366f6da4a2f710f82100bdb53ed520cc
beecher@beechersoftware.com <bill@beechersoftware.com>
2024-02-01 09:05:58 -0500

    prevent horizontal scrolling in restore panel

--- app/_lib/quilt/RestorePanel.tsx
+++ app/_lib/quilt/RestorePanel.tsx
@@ -12,7 +12,10 @@ export const RestorePanel = () => {
             <hr />
             <SidePanelH2>Saves</SidePanelH2>
             <div
-                className={`grid overflow-y-auto gap-3 grid-cols-${saveColumns}`}
+                // max-w-full
+                // Chrome & Safari have a little trouble with this layout, I think because the restore buttons can be
+                // different widths. A little truncation won't hurt anyone here, right?
+                className={`grid overflow-y-auto gap-3 grid-cols-${saveColumns} overflow-x-hidden`}
                 style={{
                     minHeight: `${minHeight}rem`,
                     // leave room for the rest of the left-side panel
